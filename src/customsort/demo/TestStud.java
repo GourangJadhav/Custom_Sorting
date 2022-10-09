584da8a11c0030fbc063bc21cb6e6de002b6bb51
GourangJadhav <gourang.java@gmail.com>
2022-10-09 19:14:25 +0530

    add new student

--- src/customsort/demo/TestStud.java
+++ src/customsort/demo/TestStud.java
@@ -11,13 +11,15 @@ public class TestStud {
 		Student s1 = new Student(1, "Parth");
 		Student s2 = new Student(4, "Chaitnya");
 		Student s3 = new Student(3, "Madhav");
+		Student s4 = new Student(5, "Sam");
 		
 		
 		TreeSet<Student> stud = new TreeSet<Student>();
 		
 		stud.add(s1);
 		stud.add(s2);
-		stud.add(s3);	
+		stud.add(s3);
+		stud.add(s4);
 		
 		System.out.println(stud);
 	}	
